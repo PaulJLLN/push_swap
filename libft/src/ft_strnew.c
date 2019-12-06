4d8be03a4b8632ba644a453d07ae8790dc6cc2bc
Paul JULLIEN <paul.jullien.code@gmail.com>
2019-12-06 23:37:06 +0100

    a

--- libft/src/ft_strnew.c
+++ libft/src/ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: paul <paul@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 10:34:44 by pauljull          #+#    #+#             */
-/*   Updated: 2019/12/06 23:31:54 by paul             ###   ########.fr       */
+/*   Updated: 2019/12/06 23:36:54 by paul             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strnew(size_t size)
 	index = 0;
 	if (!(str = (char *)malloc(sizeof(char) * (size + 1))))
 		return (NULL);
-	while (index < size)
+	while (index <= size)
 		str[index++] = 0;
 	return (str);
 }
